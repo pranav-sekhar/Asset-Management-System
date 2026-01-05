436b1adf4ae74875230f7473c8acbb45b45132fd
Pranav Sekhar <pranavsekhar103@gmail.com>
2026-01-05 11:16:05 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    //baseURL : "https://asset-management-system-hh98.onrender.com/api/",
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL : "https://asset-management-system-hh98.onrender.com/api/",
+    //baseURL: "http://127.0.0.1:8000/api/",
 
 });
 
